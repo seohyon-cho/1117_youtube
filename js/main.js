37e14a8fc6866e4e90855e84e6909a7ff1ee583d
seohyon-cho <flanique@naver.com>
2023-11-17 13:11:03 +0900

    유튜브 데이터 호출 및 동적 DOM 생성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 const api_key = 'AIzaSyDwxSLXdnfN8bTNC5fnycohdatm0Qk4dLM';
 const playlist_id = 'PLIenA9X9sYeiVsnUyEbb4wSLjTUiDSK8y';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playlist_id}&maxResults=5`;
+const num = 7;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playlist_id}&maxResults=${num}`;
+
+fetch(result_url)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+		json.items.forEach((el) => {
+			tags += `
+        <article>
+          <img src=${el.snippet.thumbnails.standard.url} />
+          <h2>${el.snippet.title}</h2>
+          <p>${el.snippet.description}</p>
+          <span>${el.snippet.publishedAt}</span>
+        </article>
+      `;
+		});
+
+		document.body.innerHTML = tags;
+	});
 
 /*
   GET 방식 : 브라우저 url을 통해서 데이터를 요청하는 방식 
